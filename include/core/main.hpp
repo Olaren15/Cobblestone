40410b558a11680cd3301adbe500df779c8afddc
Catherine Gilbert <catherine.gilbert15@gmail.com>
2020-08-19 17:19:09 -0400

    improved main function handling in windows

--- include/core/main.hpp
+++ include/core/main.hpp
@@ -2,16 +2,23 @@
 #pragma once
 
 #define SDL_MAIN_HANDLED
+#if defined  _WIN32 && !defined NDEBUG
+#include <Windows.h>
+#endif
 
 int flexMain();
 
-#if defined _WIN32 && defined NDEBUG
-#include <Windows.h>
-// ReSharper disable once CppInconsistentNaming
-int wWinMain([[maybe_unused]] _In_ HINSTANCE hInstance, [[maybe_unused]] _In_opt_ HINSTANCE hPrevInstance,
-             [[maybe_unused]] _In_ LPWSTR lpCmdLine, [[maybe_unused]] _In_ int nShowCmd) {
-#else
 int main() {
-#endif // _WIN32
+#if defined _WIN32 && !defined NDEBUG
+  // Show console output in debug mode
+  AllocConsole();
+  FILE *fDummy;
+  // ReSharper disable StringLiteralTypo
+  freopen_s(&fDummy, "CONIN$", "r", stdin);
+  freopen_s(&fDummy, "CONOUT$", "w", stderr);
+  freopen_s(&fDummy, "CONOUT$", "w", stdout);
+  // ReSharper restore StringLiteralTypo
+#endif
+
   return flexMain();
 }
