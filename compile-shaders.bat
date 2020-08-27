7e336bc6ef9b00d7553ec86a37150b8c906b4cce
Catherine Gilbert <catherine.gilbert15@gmail.com>
2020-08-27 17:34:00 -0400

    rearranged solution structure

---
+++ compile-shaders.bat
@@ -0,0 +1,2 @@
+%VK_SDK_PATH%/Bin32/glslc.exe shaders\shader.vert -o shaders\vert.spv
+%VK_SDK_PATH%/Bin32/glslc.exe shaders\shader.frag -o shaders\frag.spv
--- examples/compile-shaders.bat
+++
@@ -1,2 +0,0 @@
-%VK_SDK_PATH%/Bin32/glslc.exe shaders/shader.vert -o shaders/vert.spv
-%VK_SDK_PATH%/Bin32/glslc.exe shaders/shader.frag -o shaders/frag.spv
