a4f922fec072591732c4592a5127950ce8caf7ac
Catherine Gilbert <catherine.gilbert15@gmail.com>
2020-11-15 22:56:40 -0500

    fixed compile-shaders.sh

--- examples/example1/shaders/compile-shaders.sh
+++ examples/example1/shaders/compile-shaders.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 for vert in ./*.vert; do
-  glslc $vert -o $vert + '.spv'
+  glslc $vert -o "${vert}.spv"
 done
 
-for frag in ./*.vert; do
-  glslc $frag -o $frag + '.spv'
+for frag in ./*.frag; do
+  glslc $frag -o "${frag}.spv"
 done
