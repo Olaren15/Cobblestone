b66366edb89efca0a29b547eadf01e3155ccbd26
Catherine Gilbert <catherine.gilbert15@gmail.com>
2020-11-22 18:26:58 -0500

    fixed bash shader compilation script

--- Shaders/compile-shaders.sh
+++ Shaders/compile-shaders.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-for vert in ./*.vert; do
-  glslc $vert -o "${vert}.spv"
-done
-
-for frag in ./*.frag; do
-  glslc $frag -o "${frag}.spv"
-done
+find . -name '*.vert' -exec glslc '{}' -o '{}.spv' \;
+find . -name '*.frag' -exec glslc '{}' -o '{}.spv' \;
